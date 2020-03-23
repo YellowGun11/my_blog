cf4230155ae0546e1b65d358d70b0c5017aeb7df
YellowGun11 <yellowgun11@163.com>
2020-03-23 18:24:27 +0800

    nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,65 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import {
+    HomeOutlined,
+} from '@ant-design/icons';
 import './index.css'
 
 export default function Header(props) {
-   
+
     useEffect(() => {
         window.addEventListener('scroll', handleSetVisible);
+        console.log(1)
         return () => {
+            console.log(11)
             window.addEventListener('scroll', handleSetVisible);
         };
     })
 
     const handleSetVisible = (event) => {
-        console.log(event)
         //滚动条高度
         // let ctx = this;
         // let clientHeight = document.documentElement.clientHeight; //可视区域高度
         // let scrollHeight = document.documentElement.scrollHeight; //滚动内容高度
         let scrollTop = document.documentElement.scrollTop;  //滚动条滚动高度
         const blog_header = document.getElementsByClassName('blog_header')[0];
-        if(scrollTop>0){
-            blog_header.style.backgroundColor ='rgba(255,255,255,.95)';
-        }else{
-            blog_header.style.backgroundColor='#404040';
+        if (scrollTop > 0) {
+            blog_header.style.backgroundColor = 'rgba(255,255,255,.95)';
+            blog_header.style.color = '#666';
+        } else {
+            blog_header.style.backgroundColor = '#404040';
+            blog_header.style.color = '#404040';
         }
     }
 
     return (
         <div className="blog_header">
-            头部
+            <div className="blog_header_logo">logo</div>
+            <div className="blog_header_nav">
+                <nav>
+                    <ul className="blog_header_ul">
+                        <li>
+                            <Link to='/' exact="true"><HomeOutlined />主页</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'><HomeOutlined />分类</Link>
+                            <ul className="blog_header_category_ul">
+                                <li><Link>编程</Link></li>
+                                <li><Link>动漫</Link></li>
+                                <li><Link>教程</Link></li>
+                                <li><Link>生活</Link></li>
+                                <li><Link>兴趣</Link></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link to='/' exact="true"><HomeOutlined />登录</Link>
+                        </li>
+                        <li><Link>生活</Link></li>
+                        <li><Link>兴趣</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="blog_header_login">login11111111111</div>
         </div>
     )
 }
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import './index.css'
-import home_bg from '../../assets/img/home_bg.jpg'
 
 export default class Home extends Component {
     render() {
         return (
             <div className="blog_home">
-                <div className="blog_home_bg">
-                    <img src={home_bg} alt='' />
-                </div>
+                <div className="blog_home_bg" />
                 <div className="blog_home_title">
                     <h1 className="blog_home_title_text">Welocome To YellowGun11's Blog!</h1>
                 </div>
