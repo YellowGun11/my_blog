78345da8c80476797d2e3d8bf728df33fc0ce028
YellowGun11 <yellowgun11@163.com>
2020-03-24 16:44:50 +0800

    nav logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { HomeOutlined } from '@ant-design/icons';
 import './index.css';
+import logo from '../../assets/img/logoko.png';
 
 export default function Header(props) {
 
@@ -21,36 +22,40 @@ export default function Header(props) {
         // let scrollHeight = document.documentElement.scrollHeight; //滚动内容高度
         let scrollTop = document.documentElement.scrollTop;  //滚动条滚动高度
         const blog_header = document.getElementsByClassName('blog_header')[0];
-        const blog_header_nav = document.getElementsByClassName('blog_header_nav')[0];
+        const blog_header_ul = document.getElementsByClassName('blog_header_ul')[0];
         if (scrollTop > 0) {
             blog_header.style.backgroundColor = 'rgba(255,255,255,.95)';
             blog_header.style.boxShadow = '0 1px 40px -8px rgba(0,0,0,.5)';
             blog_header.style.color = '#666';
-            blog_header_nav.style.visibility = "visible";
+            blog_header_ul.style.visibility = "visible";
+            blog_header_ul.style.transform = "translate(-80%, -50%)";
         } else {
             blog_header.style.backgroundColor = 'rgba(0,0,0,.1)';
             blog_header.style.boxShadow = '0 rgba(0,0,0,0)';
             blog_header.style.color = '#404040';
-            blog_header_nav.style.visibility = "hidden";
+            blog_header_ul.style.visibility = "hidden";
+            blog_header_ul.style.transform = "translate(-70%, -50%)";
         }
     }
 
     return (
         <div className="blog_header">
-            <div className="blog_header_logo">logo</div>
+            <div className="blog_header_logo">
+                <img src={logo} alt='YellowGun'></img>
+            </div>
             <nav className="blog_header_nav">
                 <ul className="blog_header_ul">
                     <li>
-                        <Link to='/' exact="true"><i className="iconfont blog_header_nav_icon">&#xe635;</i>主页</Link>
+                        <Link to='/' exact="true"><i className="iconfont blog_header_nav_icon">&#xe6be;</i>主页</Link>
                     </li>
-                    <li>
-                        <Link to='/login'><i className="iconfont">&#xe64e;</i>分类</Link>
+                    <li className='blog_header_nav_category'>
+                        <Link to='/login' className="blog_header_nav_category_text"><i className="iconfont blog_header_nav_icon">&#xe64e;</i>分类</Link>
                         <ul className="blog_header_category_ul">
-                            <li><Link><HomeOutlined className="blog_header_nav_icon" />编程</Link></li>
-                            <li><Link><HomeOutlined className="blog_header_nav_icon" />动漫</Link></li>
-                            <li><Link><HomeOutlined className="blog_header_nav_icon" />教程</Link></li>
-                            <li><Link><HomeOutlined className="blog_header_nav_icon" />生活</Link></li>
-                            <li><Link><HomeOutlined className="blog_header_nav_icon" />兴趣</Link></li>
+                            <li><Link><i className="iconfont blog_header_nav_icon">&#xe670;</i>编程</Link></li>
+                            <li><Link><i className="iconfont blog_header_nav_icon">&#xe60d;</i>动漫</Link></li>
+                            <li><Link><i className="iconfont blog_header_nav_icon">&#xe6bf;</i>教程</Link></li>
+                            <li><Link><i className="iconfont blog_header_nav_icon">&#xe65a;</i>生活</Link></li>
+                            <li><Link><i className="iconfont blog_header_nav_icon">&#xe6bd;</i>兴趣</Link></li>
                         </ul>
                     </li>
                     <li>
